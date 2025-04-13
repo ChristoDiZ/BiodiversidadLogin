d2c31b668bb533b99ecf7413ad7b342c71d30675
Christopher Diaz <christopher.diaz@psinet.cl>
2025-04-13 19:28:27 -0400

    Subida de proyecto

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import SignIn from "@/app/login/page";
+
+export default function Home() {
+  return (
+    <SignIn/> //normalmente enviar a la página de biodiversidad (cambiara cuando se implemente)
+  );
+}
